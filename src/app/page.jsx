4fc72682edc9b717b3aa0f47abe785a300be5841
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-11 16:34:34 +0200

    Created body section.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
 
 export default function Home() {
   return (
     <>
+      {/* header */}
       <HomeHeader />
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://www.google.sk/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
