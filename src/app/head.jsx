daac70c3ab27d6f486b5916608bd2cd2982c20d2
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-11 15:37:18 +0200

    Created basic project.

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>Google Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Google clone created using Next.js" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
