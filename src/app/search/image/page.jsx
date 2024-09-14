d37669faea0b84c24b8301bc234629c227ddc515
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-14 18:16:46 +0200

    Removed unfinished delay

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import ImageSearchResults from "@/components/ImageSearchResults";
 
 export default async function ImageSearchPage({ searchParams }) {
-  await 
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
   );
