abaf6ef4f71a1d51f81c9491685ebaf9af436c39
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-14 18:47:44 +0200

    Add Pagination component.

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import PaginationButtons from "./PaginationButtons";
 
 export default async function ImageSearchResults({ results }) {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return (
-    <div className="pb-24 mt-4">
+    <div className="pb-40 mt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
         {results.items.map((result) => (
           <div className="" key={result.link}>
@@ -29,6 +30,9 @@ export default async function ImageSearchResults({ results }) {
             </div>
           </div>
         ))}
+      </div>
+      <div className="">
+        <PaginationButtons />
       </div>
     </div>
   );
