e25d7095f8d75b0198e7837c6bd04a98a90bf989
Pavol Padysak <pavol.padysak@student.tuke.sk>
2024-09-14 17:20:04 +0200

    Add handling of errors or no result found.

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { resolve } from "styled-jsx/css";
+import Link from "next/link";
 
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
   const data = await response.json();
-  console.log(data);
   const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl bm-4">No results found</h1>
+        <p className="text-lg">
+          Try searching something else or go back to the homepage.{"  "}
+          <Link className="text-blue-500" href="/">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
